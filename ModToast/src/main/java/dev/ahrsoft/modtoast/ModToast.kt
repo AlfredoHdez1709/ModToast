e03d30cd3e5811616c4e71748bf8ddbe5a13dc75
AlfredoHdez1709 <alfredohdez1709@gmail.com>
2021-05-06 20:23:50 -0500

    basic toast in android r

--- ModToast/src/main/java/dev/ahrsoft/modtoast/ModToast.kt
+++ ModToast/src/main/java/dev/ahrsoft/modtoast/ModToast.kt
@@ -13,6 +13,7 @@ import android.view.animation.Animation
 import android.widget.Toast
 import androidx.annotation.Nullable
 import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import dev.ahrsoft.modtoast.databinding.ModToastBinding
 
 
@@ -92,89 +93,11 @@ fun Context.ModToast(
         toastEgg.view = binding.root
         toastEgg.show()
     }else{
-        show_toast_android_r(binding, this,text, duration, style, backgroundTint, textColor, imageRes)
+        val modToast = Toast.makeText(this, text, duration)
+        modToast.show()
     }
 }
 
-fun show_toast_android_r(binding: ModToastBinding,
-                         context: Context,
-                         text: CharSequence?,
-                         duration: Int,
-                         style : ModToast,
-                         @Nullable backgroundTint: Int?,
-                         @Nullable textColor: Int?,
-                         @Nullable imageRes: Int?) {
 
 
-    val lengthSHORT = 0
-    val lengthLONG = 1
-    binding.txtToastTitle.text = text
 
-    when(style){
-        ModToast.ROUND -> {
-            binding.rootToas.setBackgroundResource(R.drawable.round)
-        }
-        ModToast.FLAT -> {
-            binding.rootToas.setBackgroundResource(R.drawable.flat)
-        }
-        ModToast.BORDER -> {
-            binding.rootToas.setBackgroundResource(R.drawable.border)
-        }
-    }
-
-    var durationToast  = 0
-
-    if (duration == lengthSHORT){
-        durationToast = 300
-    } else if (duration == lengthLONG){
-        durationToast = 600
-    }
-
-    if (textColor != null) {
-        binding.txtToastTitle.setTextColor(ContextCompat.getColor(context, textColor))
-    }
-
-    if (backgroundTint != null){
-        binding.rootToas.background.colorFilter = PorterDuffColorFilter(ContextCompat.getColor(context, backgroundTint), PorterDuff.Mode.MULTIPLY)
-    }
-
-    if (imageRes != null) {
-        binding.ivRes.setImageResource(imageRes)
-    } else {
-        binding.ivRes.visibility = View.GONE
-    }
-
-
-    val animation1 = AlphaAnimation(0f, 1f)
-    animation1.duration = durationToast.toLong()
-    animation1.fillAfter = true
-    animation1.setAnimationListener(object : Animation.AnimationListener {
-        override fun onAnimationStart(animation: Animation) {}
-        override fun onAnimationRepeat(animation: Animation) {}
-        override fun onAnimationEnd(animation: Animation) {
-            object : CountDownTimer(2250, 1) {
-                override fun onTick(millisUntilFinished: Long) {}
-                override fun onFinish() {
-                    hide_ad_hoc_toast(binding)
-                }
-            }.start()
-        }
-    })
-    binding.rootToas.visibility = View.VISIBLE
-    binding.rootToas.startAnimation(animation1)
-}
-
-
-fun hide_ad_hoc_toast(binding: ModToastBinding){
-    val animation1 = AlphaAnimation(1f, 0f)
-    animation1.duration = 300
-    animation1.fillAfter = true
-    animation1.setAnimationListener(object : Animation.AnimationListener {
-        override fun onAnimationStart(animation: Animation) {}
-        override fun onAnimationRepeat(animation: Animation) {}
-        override fun onAnimationEnd(animation: Animation) {
-            binding.rootToas.visibility = View.GONE
-        }
-    })
-   binding.rootToas.startAnimation(animation1)
-}
